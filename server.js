5e8459d1effceea6131e3accce4ff8502942220b
Rushikesh joshi <108232332+Rushikeshjoshi2024@users.noreply.github.com>
2025-02-05 16:21:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const RedisStore = require('connect-redis').default
-const redis = require('redis')
+import Redis from 'ioredis';
 const fs = require('fs')
 const path = require('path')
 
 const app = express()
-const redisClient = redis.createClient({
-    host: 'localhost', // Replace with your Redis host if using a remote service
-    port: 6379, // Default Redis port
-});
+// const redisClient = redis.createClient({
+//     host: 'localhost', // Replace with your Redis host if using a remote service
+//     port: 6379, // Default Redis port
+// });
+const RedisStore = connectRedis(session);
+const redisClient = new Redis();
 
 const allowedOrigins = ['https://ms1-git-main-rush-js-projects.vercel.app']; // Replace with your actual React app URL
 const options = {
